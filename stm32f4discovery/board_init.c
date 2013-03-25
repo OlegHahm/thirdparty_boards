8644d96ea23a8f71796db1d515e20f30b5dfbe9c
Oleg Hahm <oleg@hobbykeller.org>
2013-03-25 14:28:57 +0100

    * fixed PLL value for discovery board

--- stm32f4discovery/board_init.c
+++ stm32f4discovery/board_init.c
@@ -154,7 +154,7 @@
 
 /************************* PLL Parameters *************************************/
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
-#define PLL_M      16//25
+#define PLL_M      8//25
 #define PLL_N      336
 
 /* SYSCLK = PLL_VCO / PLL_P */
