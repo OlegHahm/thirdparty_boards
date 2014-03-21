30761a0aea49faebd46e1504e4fb4754a41ed8fe
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-03-21 10:39:10 +0100

    iot-lab_M3: implement green and orange led

--- iot-lab_M3/include/leds.h
+++ iot-lab_M3/include/leds.h
@@ -2,18 +2,25 @@
 #define LEDS_H_
 
 void leds_init(void);
-void led_on(uint32_t);
-void led_off(uint32_t);
 
 #define LED_RED_PIN         (1<<2)
-//#define LED_GREEN_PIN       (1<<5)
-//
-//#define LED_RED_ON()        led_on(LED_RED_PIN);
-//#define LED_RED_OFF()       led_off(LED_RED_PIN);
-//#define LED_RED_TOGGLE()    led_toggle(LED_RED_PIN);
-//
-//#define LED_GREEN_ON()      led_on(LED_GREEN_PIN);
-//#define LED_GREEN_OFF()     led_off(LED_GREEN_PIN);
-//#define LED_GREEN_TOGGLE()  led_toggle(LED_GREEN_PIN);
+#define LED_GREEN_PIN       (1<<5)
+#define LED_ORANGE_PIN      (1<<10)
+
+#define LED_RED_PORT        (GPIOD->ODR)
+#define LED_GREEN_PORT      (GPIOB->ODR)
+#define LED_ORANGE_PORT     (GPIOC->ODR)
+
+#define LED_RED_ON          (LED_RED_PORT &= ~LED_RED_PIN)
+#define LED_RED_OFF         (LED_RED_PORT |= LED_RED_PIN)
+#define LED_RED_TOGGLE      (LED_RED_PORT ^= LED_RED_PIN)
+
+#define LED_GREEN_ON        (LED_GREEN_PORT &= ~LED_GREEN_PIN)
+#define LED_GREEN_OFF       (LED_GREEN_PORT |= LED_GREEN_PIN)
+#define LED_GREEN_TOGGLE    (LED_GREEN_PORT ^= LED_GREEN_PIN)
+
+#define LED_ORANGE_ON       (LED_ORANGE_PORT &= ~LED_ORANGE_PIN)
+#define LED_ORANGE_OFF      (LED_ORANGE_PORT |= LED_ORANGE_PIN)
+#define LED_ORANGE_TOGGLE   (LED_ORANGE_PORT ^= LED_ORANGE_PIN)
 
 #endif
