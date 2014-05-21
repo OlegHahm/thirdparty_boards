39882ce031b40cbd6b61cc7415dcf863693dfc48
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-05-21 13:35:07 +0200

    agilefox: implement needed `fw_puts`

---
+++ agilefox/drivers/agilefox-uart.c
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2014 Oliver Hahm <oliver.hahm@inria.fr>
+ *
+ * This file subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ *
+ * @file   agilefox-uart.c
+ * @author Oliver Hahm <oliver.hahm@inria.fr>
+ */
+
+#include "stm32f10x.h"
+
+int fw_puts(char *astring, int length)
+{
+    for (int i = 0; i < length; i++) {
+        USART_SendData(USART1, astring[i]);
+
+        /* Loop until the end of transmission */
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET) {}
+    }
+
+    return length;
+}
